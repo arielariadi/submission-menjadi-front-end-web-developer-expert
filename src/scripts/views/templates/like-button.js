b09c870899c4cd7f10683c11467facacd47acae3
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2023-10-05 16:53:28 +0700

    add aria-label for favorite button

--- src/scripts/views/templates/like-button.js
+++ src/scripts/views/templates/like-button.js
@@ -1,11 +1,11 @@
 const createLikeButtonTemplate = () => `
-        <button class="favorite" id="likeButton">
+        <button aria-label="like this restaurant" class="favorite" id="likeButton">
             <i class="fa-regular fa-heart"></i> ADD TO FAVORITE
         </button>
 `;
 
 const createLikedButtonTemplate = () => `
-        <button class="favorite" id="likeButton">
+        <button aria-label="unlike this restaurant" class="favorite" id="likeButton">
             <i class="fa-solid fa-heart"></i> REMOVE FROM FAVORITE
         </button>
 `;
